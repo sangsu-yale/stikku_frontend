68aed31d7790530bc948d99187dee8c82bab9df9
idoasisay <ihannah@kakao.com>
2024-08-09 13:31:11 +0900

    구현 : gameresult의 result enum 관련한 데이터 정의

--- lib/constants/result_enum.dart
+++ lib/constants/result_enum.dart
@@ -1,8 +1,53 @@
 // ignore_for_file: constant_identifier_names
 
+import 'package:flutter/material.dart';
+import 'package:stikku_frontend/config/custom_icons.dart';
+
 enum GameResultType {
   WIN,
   CANCEL,
   LOSE,
   TIE,
+}
+
+// Extension을 사용하여 각 속성을 정의
+extension GameResultTypeProperties on GameResultType {
+  String get label {
+    switch (this) {
+      case GameResultType.CANCEL:
+        return '취';
+      case GameResultType.TIE:
+        return '무';
+      case GameResultType.LOSE:
+        return '패';
+      case GameResultType.WIN:
+        return '승';
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case GameResultType.CANCEL:
+        return Custom.umbrella__1_;
+      case GameResultType.TIE:
+        return Custom.clover__1_;
+      case GameResultType.LOSE:
+        return Custom.bookmarksimple__1_;
+      case GameResultType.WIN:
+        return Custom.star_1;
+    }
+  }
+
+  Color get color {
+    switch (this) {
+      case GameResultType.CANCEL:
+        return Colors.grey;
+      case GameResultType.TIE:
+        return Colors.green;
+      case GameResultType.LOSE:
+        return Colors.red;
+      case GameResultType.WIN:
+        return Colors.blue;
+    }
+  }
 }
