22493cf7358e37e509841bf69d580a923ca59949
idoasisay <ihannah@kakao.com>
2024-08-22 18:55:51 +0900

    권한 설정 : 사진 권한 설정 기능 등록

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stikku_frontend/controllers/user_controller.dart';
 import 'package:stikku_frontend/utils/services/isar_service.dart';
@@ -91,37 +92,35 @@ Future<void> takeScreenshot(int index, dynamic cardKeys) async {
     Get.snackbar('오류 발생', '잠시 후 다시 시도해 주세요. 문제 해결을 위해 문의해 주세요.');
   }
 
-  // // 권한 요청
-  // var status = await Permission.storage.status;
-  // if (!status.isGranted) {
-  //   print("권한 요청 팝업 띄우는 중");
-  //   status = await Permission.storage.request();
-  // }
+  // 권한 요청
+  var status = await Permission.storage.status;
+  if (!status.isGranted) {
+    status = await Permission.storage.request();
+  }
 
-  // if (status.isGranted) {
-  //   try {
-  //     // 스크린샷 캡처
-  //     RenderRepaintBoundary boundary = cardKeys[index]
-  //         .currentContext!
-  //         .findRenderObject() as RenderRepaintBoundary;
-  //     var image = await boundary.toImage(pixelRatio: 4.0); // 더 높은 해상도
-  //     ByteData? byteData =
-  //         await image.toByteData(format: ImageByteFormat.png);
-  //     Uint8List pngBytes = byteData!.buffer.asUint8List();
+  if (status.isGranted) {
+    try {
+      // 스크린샷 캡처
+      RenderRepaintBoundary boundary = cardKeys[index]
+          .currentContext!
+          .findRenderObject() as RenderRepaintBoundary;
+      var image = await boundary.toImage(pixelRatio: 4.0); // 더 높은 해상도
+      ByteData? byteData = await image.toByteData(format: ImageByteFormat.png);
+      Uint8List pngBytes = byteData!.buffer.asUint8List();
 
-  //     // 이미지 저장
-  //     final result = await ImageGallerySaver.saveImage(pngBytes);
-  //     if (result != null && result['isSuccess'] == true) {
-  //       Get.snackbar('티켓이 저장되었습니다', '갤러리를 확인해 보세요');
-  //     } else {
-  //       Get.snackbar('저장 실패', '이미지 저장에 실패했습니다. 다시 시도해 주세요.');
-  //     }
-  //   } catch (e) {
-  //     // 예외 처리
-  //     print('Error: $e'); // 디버깅을 위한 로그
-  //     Get.snackbar('오류 발생', '잠시 후 다시 시도해 주세요. 문제 해결을 위해 문의해 주세요.');
-  //   }
-  // } else {
-  //   Get.snackbar('저장 권한이 거부되었습니다', '앱 설정에서 권한을 허용해 주세요.');
-  // }
+      // 이미지 저장
+      final result = await ImageGallerySaver.saveImage(pngBytes);
+      if (result != null && result['isSuccess'] == true) {
+        Get.snackbar('티켓이 저장되었습니다', '갤러리를 확인해 보세요');
+      } else {
+        Get.snackbar('저장 실패', '이미지 저장에 실패했습니다. 다시 시도해 주세요.');
+      }
+    } catch (e) {
+      // 예외 처리
+      print('Error: $e'); // 디버깅을 위한 로그
+      Get.snackbar('오류 발생', '잠시 후 다시 시도해 주세요. 문제 해결을 위해 문의해 주세요.');
+    }
+  } else {
+    Get.snackbar('저장 권한이 거부되었습니다', '앱 설정에서 권한을 허용해 주세요.');
+  }
 }
